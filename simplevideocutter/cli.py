0d22c55d7829d372a18230ea3959925c6b7d94e9
ricekab <contact@kevinchiyantang.com>
2022-09-23 15:33:16 +0200

    WIP commit.

--- simplevideocutter/cli.py
+++ simplevideocutter/cli.py
@@ -1,7 +1,75 @@
+from datetime import datetime
+
 import click
 
+time_spec_formats = ['%H:%M:%S', '%H-%M-%S']
 
-@click.command()
+
+def _sanitize_timestamp(ctx, param, value):
+    for time_spec_format in time_spec_formats:
+        try:
+            return datetime.strptime(value, time_spec_format)
+        except ValueError:
+            pass
+    raise click.BadParameter(f'{param} needs to be in HH:MM:SS format.')
+
+
+@click.group()
 def entry():
-    click.echo("To be implemented.")
     pass
+
+
+@entry.command()
+@click.option('--source-vod', '-source',
+              type=click.Path(exists=True, file_okay=True, dir_okay=False),
+              required=True,
+              help='The source VOD to be cut.')
+@click.option('--destination-file', '-destination',
+              type=click.Path(exists=False, file_okay=True, dir_okay=False),
+              # TODO: Default value? Increment from origin name?
+              required=True,
+              help='The destination output file.')
+@click.option('--start-time', '-start',
+              type=click.DateTime(formats=time_spec_formats),
+              required=True,
+              help='The starting time stamp of the cut in HH:MM:SS format.')
+@click.option('--end-time', '-end',
+              type=click.DateTime(formats=time_spec_formats),
+              help='The end time stamp of the cut in HH:MM:SS format. This value'
+                   ' has precedence over the --duration flag.')
+@click.option('--duration', '-d',
+              type=int,
+              help='The duration (in seconds) of the cut. If --end-time is '
+                   'defined, this value is ignored.')
+@click.option('--allow-overwrite', '-force', '-f',
+              is_flag=True,
+              flag_value=True,
+              help='If this flag is given, destination file(s) can be '
+                   'overwritten.')
+def single(source_vod, destination_file, start_time, end_time, duration,
+           allow_overwrite):
+    if not end_time and not duration:
+        raise click.BadParameter(f'Either one of --end-time or --duration must '
+                                 f'be specified.')
+    click.echo('cut single')
+    click.echo(source_vod)
+    click.echo(destination_file)
+    click.echo(start_time)
+    click.echo(end_time)
+    click.echo(duration)
+    click.echo(allow_overwrite)
+
+
+@entry.command()
+def multi():
+    click.echo('TODO: Implement multi cut using timestamps file '
+               '(or multi options?)')
+
+
+@entry.command(
+    help='Start Graphical User Interface (GUI) to define video cuts. This '
+         'requires PySide2 to be installed. See the installation guide for '
+         'details on this.'
+)
+def gui():
+    click.echo('TODO: Start GUI')
--- simplevideocutter/cut.py
+++ simplevideocutter/cut.py
@@ -1,8 +1,15 @@
 """
 Video cutting / processing module.
 """
+import os
 import subprocess
+import logging
 
+import typing
+from collections import namedtuple
+from datetime import datetime
+
+_l = logging.getLogger(__name__)
 
 def generate_cut_command(input_file: str,
                          output_file: str,
@@ -54,3 +61,52 @@ def run_ffmpeg_cut_command(input_file: str,
     except subprocess.CalledProcessError as exc:
         # TODO: Custom exception here?
         raise exc
+
+
+CutSpec = namedtuple('CutSpec', ('start_offset', 'duration', 'file_name',))
+
+
+def extract_cut_specs(timestamps_file) -> typing.Iterable[CutSpec]:
+    with open(timestamps_file, 'rt') as _f:
+        data = _f.readlines()
+    data = [_line for _line in data if not _line.startswith('#')]  # filter out comments
+    specs = (_parse_line(line, idx) for idx, line in enumerate(data, start=1))
+    return [_ for _ in specs if _]
+
+
+def _parse_line(line, idx):
+    line = line.strip()
+    if line.startswith('#'):
+        return  # Ignore lines starting with #
+    args = line.split()
+    if len(args) != 3:
+        raise ValueError(f'Incorrect number of arguments. Note that spaces in filenames are '
+                         f'not allowed! Line that cause the error: {line}')
+    start_time = args[0]
+    end_time = args[1]
+    file_name = f'{idx}_{args[2]}'
+    start_time = datetime.strptime(start_time, '%H:%M:%S')
+    start_offset = start_time.hour * 60 * 60 + start_time.minute * 60 + start_time.second
+    end_time = datetime.strptime(end_time, '%H:%M:%S')
+    end_offset = end_time.hour * 60 * 60 + end_time.minute * 60 + end_time.second
+    duration = end_offset - start_offset
+    return CutSpec(start_offset=start_offset, duration=duration, file_name=file_name)
+
+
+# def cut_vod(source, timestamps_file, destination_dir):
+#     source = os.path.abspath(source)
+#     cut_specs = extract_cut_specs(timestamps_file)
+#     source_extension = os.path.splitext(source)[1]
+#     print(f'Cutting VODs from source file: {os.path.abspath(source)}')
+#     for cut_spec in cut_specs:
+#         destination = os.path.join(destination_dir, cut_spec.file_name)
+#         if not destination.endswith(source_extension):
+#             # Destination file path does not specify an extension.
+#             destination = destination + source_extension
+#         if os.path.exists(destination):
+#             print(f'Skipping "{destination}", already exists.')
+#             continue
+#         run_ffmpeg_cut_command(input_file=source,
+#                                output_file=destination,
+#                                start_offset_in_s=cut_spec.start_offset,
+#                                duration_in_s=cut_spec.duration)
